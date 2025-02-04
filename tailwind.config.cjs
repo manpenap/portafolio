e66c8f7fb6ad8dcd2cf02a66cd81fdaaa6102e9f
manuel peña <manpenap@gmail.com>
2025-02-03 22:18:29 -0300

    Implementa modo oscuro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,26 @@ module.exports = {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"
   ],
+  darkMode: 'class', 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Define aquí tu paleta de colores personalizada
+        primary: {
+          light: '#4f46e5',
+          DEFAULT: '#4338ca',
+          dark: '#3730a3',
+        },
+        background: {
+          light: '#ffffff',
+          dark: '#1f2937',
+        },
+        text: {
+          light: '#1f2937',
+          dark: '#f3f4f6',
+        },
+      },
+    },
   },
   plugins: [],
 };
